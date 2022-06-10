8e7d0c4f6386137d324479e1aa95c75a0aa9cb6b
MUYANG GUO <muyangguo@gmail.com>
2022-06-10 11:14:04 -0700

    make player smaller

--- src/components/features/MusicPlayer.js
+++ src/components/features/MusicPlayer.js
@@ -4,7 +4,7 @@ import { Slide } from "@mui/material";
 const MusicPlayer = ({disabled, isZenMode}) => {
   const players = {
     spotify:
-      '<iframe style="border-radius:20px" src="https://open.spotify.com/embed/playlist/2HeW8KNgI5nOZf0yOdO6Mf?utm_source=generator&theme=0" width="100%" height="300" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>',
+      '<iframe style="border-radius:20px" src="https://open.spotify.com/embed/playlist/2HeW8KNgI5nOZf0yOdO6Mf?utm_source=generator&theme=0" width="100%" height="200" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>',
     spotifyCompact: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2HeW8KNgI5nOZf0yOdO6Mf?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
   };
 
