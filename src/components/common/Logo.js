2c98110645a75ba21e3632d53ab90dcbbacacf5c
MUYANG GUO <muyangguo@gmail.com>
2022-06-12 02:37:43 -0700

    fix log show logic

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -4,18 +4,19 @@ import { Box } from "@mui/system";
 import IconButton from "../utils/IconButton";
 
 const Logo = ({ isFocusedMode, isMusicMode }) => {
-  if (isMusicMode) return null;
-  if (isFocusedMode)
+  if (isFocusedMode) {
+    if (isMusicMode) return null;
     return (
       <Box display="block" flexDirection="row" className="bottom-info">
-          <IconButton
-            href="https://github.com/gamer-ai/eletype-frontend/"
-            color="inherit"
-          >
-            Ele Types <KeyboardAltIcon fontSize="small" />
-          </IconButton>
+        <IconButton
+          href="https://github.com/gamer-ai/eletype-frontend/"
+          color="inherit"
+        >
+          Ele Types <KeyboardAltIcon fontSize="small" />
+        </IconButton>
       </Box>
     );
+  }
   return (
     <header>
       <h1>
