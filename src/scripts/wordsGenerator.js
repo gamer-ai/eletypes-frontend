f7321281fb9db08357c3e734ae0c1ddbc2204e4e
Abebe123000 <takashi123000@yahoo.co.jp>
2023-09-02 15:19:34 +0900

    -#1 数字、記号でタイピングできるようにする

--- src/scripts/wordsGenerator.js
+++ src/scripts/wordsGenerator.js
@@ -6,51 +6,74 @@ import {
 } from "../constants/WordsMostCommon";
 import {
   DEFAULT_DIFFICULTY,
+  NUMBER_DIFFICULTY,
+  SYMBOL_DIFFICULTY,
   ENGLISH_MODE,
   CHINESE_MODE,
   DEFAULT_WORDS_COUNT,
 } from "../constants/Constants";
 import { randomIntFromRange } from "./randomUtils";
+import { generateRandomNumChras, generateRandomSymbolChras } from "./randomCharsGenerator";
 import { VOCAB_DICTIONARIES, DICTIONARY_SOURCE_CATALOG } from "../constants/DictionaryConstants";
 
 const wordsGenerator = (wordsCount, difficulty, languageMode) => {
   if (languageMode === ENGLISH_MODE) {
     if (difficulty === DEFAULT_DIFFICULTY) {
-        const EnglishWordList = [];
-        for (let i = 0; i < DEFAULT_WORDS_COUNT; i++) {
-          const rand = randomIntFromRange(0, 550);
-          EnglishWordList.push(COMMON_WORDS[rand]);
-        }
-        return EnglishWordList;
+      const EnglishWordList = [];
+      for (let i = 0; i < DEFAULT_WORDS_COUNT; i++) {
+        const rand = randomIntFromRange(0, 550);
+        EnglishWordList.push(COMMON_WORDS[rand]);
+      }
+      return EnglishWordList;
     }
+
+    if (difficulty === NUMBER_DIFFICULTY) {
+      const words = [];
+      for (let i = 0; i < wordsCount; i++) {
+        const word = generateRandomNumChras(2, 7);
+        words.push({ key: word, val: word });
+      }
+      return words;
+    }
+
+    if (difficulty === SYMBOL_DIFFICULTY) {
+      const words = [];
+      for (let i = 0; i < wordsCount; i++) {
+        const word = generateRandomSymbolChras(2, 7);
+        words.push({ key: word, val: word });
+      }
+      return words;
+    }
+
+    // hard
     const randomWordsGenerated = randomWords({ exactly: wordsCount, maxLength: 7 });
     const words = [];
     for (let i = 0; i < wordsCount; i++) {
-        words.push({key: randomWordsGenerated[i], val: randomWordsGenerated[i]});
-      }
+      words.push({ key: randomWordsGenerated[i], val: randomWordsGenerated[i] });
+    }
     return words;
   }
   return ["something", "went", "wrong"];
 };
 
-const chineseWordsGenerator = ( difficulty, languageMode) => {
+const chineseWordsGenerator = (difficulty, languageMode) => {
   if (languageMode === CHINESE_MODE) {
-    if (difficulty === DEFAULT_DIFFICULTY){
+    if (difficulty === DEFAULT_DIFFICULTY) {
       const ChineseWordList = [];
       for (let i = 0; i < DEFAULT_WORDS_COUNT; i++) {
         const rand = randomIntFromRange(0, 5000);
-        if (COMMON_CHINESE_WORDS[rand] && COMMON_CHINESE_WORDS[rand].val){
+        if (COMMON_CHINESE_WORDS[rand] && COMMON_CHINESE_WORDS[rand].val) {
           ChineseWordList.push(COMMON_CHINESE_WORDS[rand]);
         }
       }
-  
+
       return ChineseWordList;
     }
 
     const ChineseIdiomsList = [];
     for (let i = 0; i < DEFAULT_WORDS_COUNT; i++) {
       const rand = randomIntFromRange(0, 5000);
-      if (COMMON_CHINESE_IDIOMS_WORDS[rand] && COMMON_CHINESE_IDIOMS_WORDS[rand].val){
+      if (COMMON_CHINESE_IDIOMS_WORDS[rand] && COMMON_CHINESE_IDIOMS_WORDS[rand].val) {
         ChineseIdiomsList.push(COMMON_CHINESE_IDIOMS_WORDS[rand]);
       }
     }
@@ -71,4 +94,4 @@ const wordsCardVocabGenerator = (vocabSource, chapter) => {
   return wordsList;
 }
 
-export { wordsGenerator, chineseWordsGenerator, wordsCardVocabGenerator};
+export { wordsGenerator, chineseWordsGenerator, wordsCardVocabGenerator };
